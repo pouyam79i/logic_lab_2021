de3b0da3027e17695660b52fefd3de4b73f22e12
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-05-01 10:26:56 +0430

    finial update and debug for assigment 5

--- assignment-05/tb_function4x1.v
+++ assignment-05/tb_function4x1.v
@@ -24,7 +24,7 @@
 `timescale 1 ns/1 ns
 
 
-module tb_function4x1 ();
+module tb_function4x1();
 
 	
 reg a;
@@ -45,6 +45,10 @@ wire f;
 		a <= 1'b0; b <= 1'b0; c <= 1'b1; d <= 1'b0;
 		# 10 ;
 		a <= 1'b0; b <= 1'b1; c <= 1'b1; d <= 1'b1;
+		# 10 ;
+		a <= 1'b1; b <= 1'b0; c <= 1'b1; d <= 1'b1;
+		# 10 ;
+		$finish;
 	end
 
 endmodule
