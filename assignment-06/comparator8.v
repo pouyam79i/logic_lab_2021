5fee5267ff6eafcfd7969b5bcf99f4f4ec278f91
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-05-02 17:39:49 +0430

    update

--- assignment-06/comparator8.v
+++ assignment-06/comparator8.v
@@ -30,17 +30,21 @@ module comparator8 (
 	output 		et ,
 	output 		gt
 );
+
+	// wires
 	wire l1,l2,e1,e2,g1,g2;
 	wire [2:0] inA,inB;
 
-	
+	// assignment of wires
 	assign inA[1:0] = A[7:6];
 	assign inB[1:0] = B[7:6];
 	assign inA[2] = 1'b0;
 	assign inB[2] = 1'b0;
-		comparator3 com1(A[2:0],B[2:0],l,e,g,l1,e1,g1);
-		comparator3 com2(A[5:3],B[5:3],l1,e1,g1,l2,e2,g2);
-		comparator3	com3(inA[2:0],inB[2:0],l2,e2,g2,lt,et,gt);
+
+	// Combining 3-bit comapators
+	comparator3 com1(A[2:0],B[2:0],l,e,g,l1,e1,g1);
+	comparator3 com2(A[5:3],B[5:3],l1,e1,g1,l2,e2,g2);
+	comparator3	com3(inA[2:0],inB[2:0],l2,e2,g2,lt,et,gt);
 	
 					
 	
