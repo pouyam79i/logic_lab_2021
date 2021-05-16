4e6dd08b597088b042438f7279313bd906a2e271
ArsalanAa <58984008+ArsalanAa@users.noreply.github.com>
2021-05-16 15:26:42 +0430

    Update adder_subtractor_4bit_delay.v
    delayed

--- assignment-07/adder_subtractor_4bit_delay.v
+++ assignment-07/adder_subtractor_4bit_delay.v
@@ -31,8 +31,12 @@ module adder_subtractor_4bit_delay (
 	output cout
 );
 
-	/* write your code here */
 	
-	/* write your code here */
+		wire c1,c2,c3;
+		addsub_delay add1(A[0],B[0],Sell,Sell,S[0],c1);
+		addsub_delay add2(A[1],B[1],Sell,c1,S[1],c2);
+		addsub_delay add3(A[2],B[2],Sell,c2,S[2],c3);
+		addsub_delay add4(A[3],B[3],Sell,c3,S[3],Cout);
+
 
 endmodule
