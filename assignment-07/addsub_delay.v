234705ccac94ed739924007f84203bf47f1de819
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-05-16 11:53:44 +0430

    add and addsub completed - not debuged

--- assignment-07/addsub_delay.v
+++ assignment-07/addsub_delay.v
@@ -32,8 +32,20 @@ module add_sub_delay (
 	output cout
 );
 
-	/* write your code here */
-	
-	/* write your code here */
+	// Condition of second input 'b'
+	wire bc;
+	xor #10 bXor(bc, b, sel);
+
+	// Sum
+	wire xi;
+	xor #10 abXor(xi, a, bc);
+	xor #10 sum(sum, cin, xi);
+
+	// Carry out
+	wire o1, p1, p2;
+	or  #5 abOr(o1, a, bc);
+	and #5 ocAnd(p1, o1, cin);
+	and #5 abAnd(p2, a, bc);
+	or  #5 carryOut(cout, p1, p2);
 
 endmodule
