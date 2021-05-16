ce6e646e2a6daf96a31a95d1839f33d9ea64c3d8
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-05-16 17:05:08 +0430

    update

--- assignment-07/tb_adder_subtractor_4bit.v
+++ assignment-07/tb_adder_subtractor_4bit.v
@@ -47,61 +47,61 @@ module tb_adder_subtractor_4bit ();
 	initial 
 		begin
 		// A>0 & B>0 & add
-		A =1;
+		A = 1;
 		B = 2;
-		Sell = 0;
+		sel = 0;
 		#200;
 		A = 2;
 		B = 3;
-		Sell = 0;
+		sel = 0;
 		#200;
 		A = 10;
 		B = 12;
-		Sell = 0;
+		sel = 0;
 		#200;
 		A = 5;
 		B = 6;
-		Sell = 0;
+		sel = 0;
 		#200;
 		// A>0 & B>0 & Subtract
 		A = 5;
 		B = 2;
-		Sell = 1;
+		sel = 1;
 		#200;
 		A = 2;
 		B = 5;
-		Sell = 1;
+		sel = 1;
 		#200;
 		A = 4;
 		B = 4;
-		Sell = 1;
+		sel = 1;
 		#200;
 		//A<0 & B>0 & add & subtract
 		A = -5;
 		B = 2;
-		Sell = 0;
+		sel = 0;
 		#200;
 		A = -3;
 		B = 2;
-		Sell = 1;
+		sel = 1;
 		#200;		
 		//A>0 & B<0 & add & subtract
 		A = 5;
 		B = -2;
-		Sell = 0;
+		sel = 0;
 		#200;		
 		A = 5;
 		B = -2;
-		Sell = 1;
+		sel = 1;
 		#200;
 		//A<0 & B<0 & add & subtract
 		A = -5;
 		B = -2;
-		Sell = 0;
+		sel = 0;
 		#200;		
 		A = -5;
 		B = -2;
-		Sell = 1;
+		sel = 1;
 		#200;		
 		$finish;
 	end
