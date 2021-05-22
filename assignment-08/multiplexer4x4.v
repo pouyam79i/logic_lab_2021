b524c784bee96db8196567dc3dcfb692ce56344f
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-05-22 22:17:09 +0430

    comments of assigment 8 - update

--- assignment-08/multiplexer4x4.v
+++ assignment-08/multiplexer4x4.v
@@ -2,16 +2,19 @@
 --  Computer Architecture Course, Laboratory Sources 
 --  Amirkabir University of Technology (Tehran Polytechnic)
 --  Department of Computer Engineering (CE-AUT)
---  https://ce.aut.ac.ir
+--  https://ce[dot]aut[dot]ac[dot]ir
 --  *******************************************************
 --  All Rights reserved (C) 2019-2020
 --  *******************************************************
---  Student ID  : 
---  Student Name: 
---  Student Mail: 
+--  Student ID  : 9829039	
+--  Student Name: Pouya Mohammadi
+--  Student Mail: pouyamohammadyirbu@gmail.com
 --  *******************************************************
 --  Additional Comments:
 --
+--	Teamate information:
+--	Mehran Aksari
+--	9831007
 --*/
 
 /*-----------------------------------------------------------
